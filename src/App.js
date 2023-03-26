0ff028575ff0525616484a68081c70a3591f16d0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-27 00:41:42 +0100

    fix: refactor code

--- src/App.js
+++ src/App.js
@@ -62,12 +62,14 @@ function App() {
           <h3>{title}</h3>
           <h4>{company}</h4>
           <p className='job-date'>{dates}</p>
-          {duties.map((duty, index) => (
-            <div key={index} className='job-desc'>
-              <FaAngleDoubleRight className='job-icon' />
-              <p>{duty}</p>
-            </div>
-          ))}
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
         </article>
       </div>
     </section>
